998c7d030a7104d7bbbc081f789edf58102e6a55
Lee-Scott <leehscott24@gmail.com>
2023-02-23 12:11:43 -0800

    start of a payroll client

---
+++ FFS_Payroll/src/main/java/com/familyfirstsofteawre/FFS_Payroll/FfsPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.familyfirstsofteawre.FFS_Payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfsPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfsPayrollApplication.class, args);
+	}
+
+}
---
+++ FFS_Payroll/src/test/java/com/familyfirstsofteawre/FFS_Payroll/FfsPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyfirstsofteawre.FFS_Payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfsPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
